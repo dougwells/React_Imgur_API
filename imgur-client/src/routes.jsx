90cff778de7fd6332e0d59ef4240cc5c5a6d5be4
dougwells <dougwells@gmail.com>
2015-12-19 07:43:33 -0700

    Set up routes.  Main.jsx serves as " Header component" .

---
+++ imgur-client/src/routes.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactRouter = require('react-router'); //actual react router library
+var HashHistory = require('react-router/lib/hashhistory'); //how React keeps track of 'page' user is on
+var Main = require('./components/main');
+
+var Router = ReactRouter.Router; //actual router deciding what content to show on page at any given time
+var Route = ReactRouter.Route;  //object used to configure .Router
+
+
+//create a routes object (was var routes = ...)
+module.exports = (
+  <Router history={new HashHistory} >
+
+    <Route path='/' component= {Main}>
+
+    </Route>
+
+  </Router>
+)
