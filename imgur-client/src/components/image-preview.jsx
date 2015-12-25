e8c8935e952ed57e85a6d7a51778d688201f6590
dougwells <dougwells@gmail.com>
2015-12-25 07:28:32 -0700

    L61: Video play/loop on mouse hover

--- imgur-client/src/components/image-preview.jsx
+++ imgur-client/src/components/image-preview.jsx
@@ -2,10 +2,19 @@ var React = require('react');
 
 module.exports = React.createClass({
 
+  getInitialState: function(){
+    return {
+      hovering: false
+    }
+  },
   render: function(){
-// ?? why just this.image???
-    return <div className="image-preview">
-      {this.image()}
+// ?? why just this.image???  B/C it is a helper function below
+    return <div
+      className="image-preview"
+      onMouseEnter= {this.handleMouseEnter}
+      onMouseLeave={this.handleMouseLeave}
+      >
+      {this.props.animated && this.state.hovering ? this.video() : this.image() }
     </div>
   },
 
@@ -14,5 +23,18 @@ module.exports = React.createClass({
   image: function(){
     var link='http://i.imgur.com/'+this.props.id+'h.jpeg';
     return <img src={link} />
+  },
+  video: function(){
+    return <div>
+      <video preload="auto" autoPlay="autoPlay" loop="loop" webkit-plays-inline>
+        <source src={this.props.mp4} type="video/mp4"></source>
+      </video>
+    </div>
+  },
+  handleMouseEnter: function(){
+    this.setState({hovering:true})
+  },
+  handleMouseLeave: function(){
+    this.setState({hovering: false})
   }
 });
